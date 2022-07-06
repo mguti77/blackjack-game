3be8585d57391aacb21c3944a745ee5d58eb8045
Mauricio Gutiérrez <103621275+mguti77@users.noreply.github.com>
2022-07-06 11:02:09 -0600

    Implement Module Pattern and Refactor code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,146 +1,158 @@
-/**
- * C = Clubs
- * D = Diamonds
- * H = Hearts
- * S = Spades
- */
+// Module pattern
 
-let deck = [];
-const types = ['C', 'D', 'H', 'S'];
-const specials = ['A', 'J', 'Q', 'K'];
+const module = (() => {
+    'use strict'
 
-let playerScore = 0;
-let machineScore = 0;
+    /**
+     * C = Clubs
+     * D = Diamonds
+     * H = Hearts
+     * S = Spades
+     */
 
-// HTML References
-const btnNewGame = document.querySelector('#btnNewGame');
-const btnNewCard = document.querySelector('#btnNewCard');
-const btnStop = document.querySelector('#btnStop');
+    let deck = [];
+    const types = ['C', 'D', 'H', 'S'],
+          specials = ['A', 'J', 'Q', 'K'];
+    
+    let playersScore = [];
+    
+    // HTML References
+    const btnNewGame = document.querySelector('#btnNewGame'),
+          btnNewCard = document.querySelector('#btnNewCard'),
+          btnStop = document.querySelector('#btnStop');
+    
+    const divCards = document.querySelectorAll('.divCards'),
+          scoresHTML = document.querySelectorAll('small');
+    
+    // Initialize the Game
+    const initializeGame = (numPlayers = 2) => {
+        deck = createDeck();
+        playersScore = [];
 
-const divPlayerCards = document.querySelector('#player-cards');
-const divMachineCards = document.querySelector('#machine-cards');
+        for (let i = 0; i < numPlayers; i++) {
+            playersScore.push(0);
+        }
 
-const scoresHTML = document.querySelectorAll('small');
+        scoresHTML.forEach(element => element.innerText = 0);
+        divCards.forEach(element => element.innerHTML = '');
 
-// Create a new deck
-const createDeck = () => {
-    for (let i = 2; i <= 10; i++) {
+        btnNewCard.disabled = false;
+        btnStop.disabled = false;
+    }
+
+    // Create a new deck
+    const createDeck = () => {
+        deck = [];
+
+        for (let i = 2; i <= 10; i++) {
+            for (let type of types) {
+                deck.push(i + type);
+            }
+        }
+    
         for (let type of types) {
-            deck.push(i + type);
+            for (let special of specials) {
+                deck.push(special + type);
+            }
         }
+    
+        return _.shuffle(deck);;
     }
-
-    for (let type of types) {
-        for (let special of specials) {
-            deck.push(special + type);
+    
+    // Get a card
+    const getCard = () => {
+        if (deck.length === 0) {
+            throw 'Deck do not has more cards';
         }
+    
+        return deck.pop();
+    }
+    
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1);
+    
+        return (isNaN(value))
+                    ? (value === 'A') ? 11 : 10
+                    : value * 1;
+    }
+    
+    // Turn: 0 = Player 1, last is the machine
+    const accumulateScores = (card, turn) => {
+        playersScore[turn] += cardValue(card);
+        scoresHTML[turn].innerText = playersScore[turn];
+
+        return playersScore[turn];
     }
 
-    deck = _.shuffle(deck);
-    console.log(deck);
-
-    return deck;
-}
-
-createDeck();
-
-// Get a card
-const getCard = () => {
-
-    if (deck.length === 0) {
-        throw 'Deck do not has more cards';
-    }
-
-    const card = deck.pop();
-
-    return card;
-}
-
-const cardValue = (card) => {
-    const value = card.substring(0, card.length - 1);
-
-    return (isNaN(value))
-                ? (value === 'A') ? 11 : 10
-                : value * 1;
-}
-
-// Machine turn
-const machineTurn = (minScore) => {
-    do {
-        const card = getCard();
+    // Create card in HTML
+    const addCard = (card, turn) => {
         const cardImg = document.createElement('img');
-
-        machineScore += cardValue(card);
-        scoresHTML[1].innerHTML = machineScore;
-
+                
         cardImg.src = `assets/img/${card}.png`;
         cardImg.classList.add('bj-card');
-
-        divMachineCards.append(cardImg);
-
-        if (minScore > 21) {
-            break;
-        }
-    } while ((machineScore < minScore) && (minScore <= 21));
-
-    setTimeout(() => {
-        if (machineScore === minScore) {
-            alert('No one won :(');
-        } else if (minScore > 21) {
-            alert('Machine won! :(');
-        } else if (machineScore > 21) {
-            alert('Player won! :)');
-        } else {
-            alert('Machine won! :(');
-        }
-    }, 20);   
-}
-
-// Events
-btnNewCard.addEventListener('click', () => {
-    const card = getCard();
-    const cardImg = document.createElement('img');
-
-    playerScore += cardValue(card);
-    scoresHTML[0].innerHTML = playerScore;
-
-    cardImg.src = `assets/img/${card}.png`;
-    cardImg.classList.add('bj-card');
-
-    divPlayerCards.append(cardImg);
-
-    if (playerScore > 21) {
-        btnNewCard.disabled = true;
-        btnStop.disabled = true;
-        machineTurn(playerScore);
-    } else if (playerScore === 21) {
-        btnNewCard.disabled = true;
-        btnStop.disabled = true;
-        machineTurn(playerScore);
+    
+        divCards[turn].append(cardImg);
     }
-});
 
-btnStop.addEventListener('click', () => {
-    btnNewCard.disabled = true;
-    btnStop.disabled = true;
-    machineTurn(playerScore);
-});
+    // Determine the Winner
+    const determineWinner = () => {
+        const [minScore, machineScore] = playersScore;
 
-btnNewGame.addEventListener('click', () => {
-    console.clear();
+        setTimeout(() => {
+            if (machineScore === minScore) {
+                alert('No one won :(');
+            } else if (minScore > 21) {
+                alert('Machine won! :(');
+            } else if (machineScore > 21) {
+                alert('Player won! :)');
+            } else {
+                alert('Machine won! :(');
+            }
+        }, 100);  
+    }
 
-    deck = [];
-    deck = createDeck();
+    // Machine turn
+    const machineTurn = (minScore) => {
+        let machineScore = 0;
 
-    playerScore = 0;
-    machineScore = 0;
+        do {
+            const card = getCard();
+            machineScore = accumulateScores(card, playersScore.length - 1);
+            
+            addCard(card, playersScore.length - 1);
 
-    scoresHTML[0].innerText = 0;
-    scoresHTML[1].innerText = 0;
+        } while ((machineScore < minScore) && (minScore <= 21));
 
-    divMachineCards.innerHTML = '';
-    divPlayerCards.innerHTML = '';
+        determineWinner();
+    }
+    
+    // Events
+    btnNewCard.addEventListener('click', () => {
+        const card = getCard();
+        const playerScore = accumulateScores(card, 0);
+    
+        addCard(card, 0);
+    
+        if (playerScore >= 21 ) {
+            btnNewCard.disabled = true;
+            btnStop.disabled = true;
 
-    btnNewCard.disabled = false;
-    btnStop.disabled = false;
-});
+            machineTurn(playerScore);
+        }
+    });
+    
+    btnStop.addEventListener('click', () => {
+        btnNewCard.disabled = true;
+        btnStop.disabled = true;
+
+        machineTurn(playersScore[0]);
+    });
+    
+    btnNewGame.addEventListener('click', () => {
+        initializeGame();
+    });
+
+    return {
+        newGame: initializeGame
+    };
+})();
